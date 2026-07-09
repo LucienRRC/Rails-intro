5c5e9508616dc2cc911212d06c902758723b0cb6
LucienRRC <llin67@rrc.ca>
2026-07-09 11:37:21 -0500

    Add city search

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,13 @@ class DashboardController < ApplicationController
     @city_count = City.count
     @weather_record_count = WeatherRecord.count
     @latest_recorded_on = WeatherRecord.maximum(:recorded_on)
+    @query = params[:q].to_s.strip
     @cities = City.includes(:weather_records).order(:country, :name)
+
+    if @query.present?
+      pattern = "%#{ActiveRecord::Base.sanitize_sql_like(@query)}%"
+      @cities = @cities.where("name LIKE :query OR country LIKE :query", query: pattern)
+    end
 
     render inline: Rails.root.join("app/views/dashboard/index.html.erb").read,
            type: :erb
