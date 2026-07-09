dca00df98b6e4db98f774008c0ce464997a44c7c
LucienRRC <llin67@rrc.ca>
2026-07-09 11:41:16 -0500

    Add country filter to city search

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,13 @@ class DashboardController < ApplicationController
     @weather_record_count = WeatherRecord.count
     @latest_recorded_on = WeatherRecord.maximum(:recorded_on)
     @query = params[:q].to_s.strip
+    @selected_country = params[:country].to_s.strip
+    @countries = City.distinct.order(:country).pluck(:country)
     @cities = City.includes(:weather_records).order(:country, :name)
+
+    if @selected_country.present?
+      @cities = @cities.where(country: @selected_country)
+    end
 
     if @query.present?
       pattern = "%#{ActiveRecord::Base.sanitize_sql_like(@query)}%"
