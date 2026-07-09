c5cfe31745803ddc4ad5507a373f4a0e22326199
LucienRRC <llin67@rrc.ca>
2026-07-08 23:41:23 -0500

    Add weather records table to city page

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,6 +3,7 @@ class CitiesController < ApplicationController
     @city = City.find(params[:id])
     @record_count = @city.weather_records.count
     @latest_recorded_on = @city.weather_records.maximum(:recorded_on)
+    @weather_records = @city.weather_records.order(recorded_on: :desc)
 
     render inline: Rails.root.join("app/views/cities/show.html.erb").read,
            type: :erb
