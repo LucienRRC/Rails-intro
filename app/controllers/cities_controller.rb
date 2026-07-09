aab914f5c147ef60e9707f233fca9e20d39cd2fe
LucienRRC <llin67@rrc.ca>
2026-07-09 11:50:00 -0500

    Paginate city weather records

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,7 +3,13 @@ class CitiesController < ApplicationController
     @city = City.find(params[:id])
     @record_count = @city.weather_records.count
     @latest_recorded_on = @city.weather_records.maximum(:recorded_on)
-    @weather_records = @city.weather_records.order(recorded_on: :desc)
+    @records_per_page = 10
+    @current_page = [params[:page].to_i, 1].max
+    @total_pages = (@record_count.to_f / @records_per_page).ceil
+    @weather_records = @city.weather_records
+                            .order(recorded_on: :desc)
+                            .limit(@records_per_page)
+                            .offset((@current_page - 1) * @records_per_page)
 
     render inline: Rails.root.join("app/views/cities/show.html.erb").read,
            type: :erb
